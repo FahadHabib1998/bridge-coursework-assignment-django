44f88283830c10f32ab599a25fdf4e0bb2541bed
Fahad Habib Al Waheedi <fhabib5088@gmail.com>
2020-05-30 11:11:27 +0100

    linking minor change

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('blog',views.blogpage, name='blogpage')
+    path('blog/',views.blogpage, name='blogpage')
 ]
