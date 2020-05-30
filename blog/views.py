2ea6d9a2ad67ebb0e732c73ed76ed88e451486de
Fahad Habib Al Waheedi <fhabib5088@gmail.com>
2020-05-30 10:56:58 +0100

    linking mainpage and blogpage

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def mainpage(request):
     return render(request,'blog/mainpage.html',{})
+
+def blogpage(request):
+    posts = BlogPost.objects
+    return render(request,'blog/blogpage.html',{'posts': posts})
