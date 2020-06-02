e2a7884cbf39cd15b2eda60c766ad1813bcca975
Fahad Habib Al Waheedi <fhabib5088@gmail.com>
2020-06-02 17:39:06 +0100

    Start CV

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import BlogPost
+from .models import CVElem
 
 admin.site.register(BlogPost)
+admin.site.register(CVElem)
--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+class CVElem(models.Model):
+    title = models.CharField(max_length=200)
+    content =  models.TextField()
+    addDate = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
