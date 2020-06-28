7b104b6e7e68b0cc5f8efd7f7451f9fe596fd25f
Fahad Habib Al Waheedi <fhabib5088@gmail.com>
2020-06-28 17:32:39 +0100

    security

--- myweb/settings.py
+++ myweb/settings.py
@@ -20,16 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY =  os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
-
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_SECURE = True
 
 
 # Application definition
