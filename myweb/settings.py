e2b34b93cd549720915c4520eadf862cfe9e3587
Fahad Habib Al Waheedi <fhabib5088@gmail.com>
2020-06-28 17:37:03 +0100

    security error

--- myweb/settings.py
+++ myweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ['SECRET_KEY']
+SECRET_KEY = 'tf1f*-&x&&)i(%t7c0yk2xi(8so$t3x6bpe7uequr80tx%_@dy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
